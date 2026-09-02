815770e7c6b0ffa79ed3d1a7506ee0c735ea6a34
Mateus Huster <mateushuster@gmail.com>
2026-09-02 21:51:15 +0000

    aula_02-09

--- ex1-biblioteca-v1-basico/src/main/java/com/bcopstein/ex1biblioeca/Controller.java
+++ ex1-biblioteca-v1-basico/src/main/java/com/bcopstein/ex1biblioeca/Controller.java
@@ -1,11 +1,22 @@
 package com.bcopstein.ex1biblioeca;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    private List<Livro> listaLivros;
+
+    public Controller(){
+        this.listaLivros = new LinkedList<>();
+        this.listaLivros.add(new Livro(1, "Dom Casmurro", "Machado de Assis", 1899));
+        this.listaLivros.add(new Livro(2, "1984", "George Orwell", 1949));
+        this.listaLivros.add(new Livro(3, "O Senhor dos Anéis", "J.R.R. Tolkien", 1954));
+    }
+
     @GetMapping("")
     @CrossOrigin(origins = "*")
     public String mensagemDeBemVindo() {
@@ -14,7 +25,41 @@ public class Controller {
 
     @GetMapping("/livros")
     @CrossOrigin(origins = "*")
-    public String listaLivros() {
-        return "Esta e a lista de livros";
+    public List<Livro> getListaLivros() {
+        return this.listaLivros;
+    }
+
+    @PostMapping("/livros")
+    @CrossOrigin(origins = "*")
+    public void adicionaLivro(@RequestBody Livro livro){
+        this.listaLivros.add(livro);
+    }
+
+    @GetMapping("/titulo")
+    @CrossOrigin(origins = "*")
+    public String getTitulosLivros(){
+        StringBuilder titulos = new StringBuilder();
+
+        for(Livro livro : this.listaLivros){
+            titulos.append(livro.getTitulo());
+        }
+
+        return titulos.toString();
+    }
+
+    @GetMapping("/livrosporano")
+    @CrossOrigin(origins = "*")
+    public List<Livro> livrosPorAno(@RequestParam(value = "ano") String ano){
+        return livros.stream()
+        .filter(livro -> livro.getAno().equals(ano.trim()))
+        .toList();
+    }
+
+    @GetMapping("/livrosdesatualizados/{ano}")
+    @CrossOrigin(origins = "*")
+    public List<Livro> livrosDesatualizados(@PathVariable(value="ano") int ano){
+        return livrosDesatualizados.stream()
+        .filter(livro -> livro.getAno() < ano)
+        .toList();
     }
 }
---
+++ ex1-biblioteca-v1-basico/src/main/java/com/bcopstein/ex1biblioeca/Livro.java
@@ -0,0 +1,21 @@
+package com.bcopstein.ex1biblioeca;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public Livro(int id, String titulo, String autor, int ano) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    public int getld() { return id; }
+    public String getTitulo() { return titulo; }
+    public String getAutor() { return autor; }
+    public int getAno() { return ano; }
+
+}
